513d5a05bc070256c0b648ae8591356649aa6768
Gs_Sidhu <102853351+guru-desi@users.noreply.github.com>
2025-12-30 14:51:42 +0530

    Create Assign_5__Task_2.py

---
+++ Assign_5__Task_2.py
@@ -0,0 +1,15 @@
+# Assignment 5  # Task 2
+
+# Demonstrate List Slicing
+
+# create List -> Extract First 5 -> Reverse it
+
+def slice():
+    list = [1,2,3,4,5,6,7,8,9,10]
+    FirstFive = list[0:5]
+    reverse = FirstFive[::-1]
+    print("Origibal List : ",list)
+    print("Extracted First Five element : ",FirstFive)
+    print("Reversed Extracted Elements : ",reverse)
+slice()    
+ 
